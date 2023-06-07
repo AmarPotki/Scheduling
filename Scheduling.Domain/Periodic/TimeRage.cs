a87598f6317bd8fa011a0ae2e63fee805c8148e8
amar potki <amar.potki@gmail.com>
2023-06-07 11:23:12 +0330

    Add project files.

---
+++ Scheduling.Domain/Periodic/TimeRage.cs
@@ -0,0 +1,8 @@
+﻿namespace Scheduling.Domain.Periodic;
+
+public class TimeRage : Range<TimeOnly>
+{
+    public TimeRage(TimeOnly minimum, TimeOnly maximum) : base(minimum, maximum)
+    {
+    }
+}
